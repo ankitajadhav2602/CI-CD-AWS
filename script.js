ef7e4d1a4e1a24ad02992f5c88b28a3b1572cdf0
ankitajadhav2602 <ankitajadhav2602@gmail.com>
2026-01-29 13:38:03 +0530

    files added

---
+++ script.js
@@ -0,0 +1,5 @@
+const text = `I am an Associate Software Engineer with a strong interest in cloud computing and backend technologies. I recently earned the AWS Certified Cloud Practitioner certification, strengthening my understanding of AWS core services, cloud architecture, security, and pricing models.
+
+I enjoy building and learning through hands-on projects, especially on AWS, where I focus on creating scalable and efficient solutions. My technical foundation includes C, C++, Java, SQL, DBMS, along with experience using Git, GitHub, and Docker.`;
+
+document.getElementById("content").innerText = text;
